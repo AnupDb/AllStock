c9a1462fa0f248a55636e41ab6aacdacc3ae03b1
Ritik <ritik.katlana1005@gmail.com>
2019-04-07 23:14:18 +0530

    Added some CSS for result display

--- src/Results.js
+++ src/Results.js
@@ -2,10 +2,17 @@ import React from "react";
 import "./results.scss";
 const Results = ({ images }) => {
   return (
-    <div class="flex-images">
-      {images.map(image => (
-        <img src={image.previewURL} key={image.previewURL} />
-      ))}
+    <div className="flex-images">
+    {images.map(image => (
+
+      <div className="results-returned">
+        <img src={image.previewURL} key={image.previewURL} className="image-result"/>
+        <div className="foot-result">
+          <strong>...</strong>
+        </div>
+      </div>
+
+    ))}
     </div>
   );
 };
